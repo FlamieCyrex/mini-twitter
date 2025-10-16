ae847b763f884ce0e8ba440a6b67092abb23d646
FlamieCyrex <wave-5@yandex.ru>
2025-10-16 14:29:00 +0300

    hotfix: fix AuthController for testing auth

---
+++ src/main/java/ru/zaletnev/mini_twitter/contoller/AuthController.java
@@ -0,0 +1,37 @@
+package ru.zaletnev.mini_twitter.contoller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.*;
+import ru.zaletnev.mini_twitter.model.dto.RegisterRequest;
+import ru.zaletnev.mini_twitter.model.dto.UserResponse;
+import ru.zaletnev.mini_twitter.service.UserService;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final UserService userService;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserResponse register(@Valid @RequestBody RegisterRequest registerRequest) {
+        return userService.register(registerRequest);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
