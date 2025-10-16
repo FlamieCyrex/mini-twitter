c2ddb0f50cf75897fa2494cedc64c340b6317f5e
FlamieCyrex <wave-5@yandex.ru>
2025-10-16 13:45:48 +0300

    hotfix: fix class name

--- src/main/java/ru/zaletnev/mini_twitter/model/dto/UserResponse.java
+++ src/main/java/ru/zaletnev/mini_twitter/model/dto/UserResponse.java
@@ -2,7 +2,7 @@ package ru.zaletnev.mini_twitter.model.dto;
 
 import java.util.UUID;
 
-public record UserResponce(
+public record UserResponse(
         UUID id,
         String username,
         String displayName
