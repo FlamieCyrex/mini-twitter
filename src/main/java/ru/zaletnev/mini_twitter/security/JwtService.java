e9a80792bc03105985d8657ebfaae64076abf55a
FlamieCyrex <wave-5@yandex.ru>
2025-10-16 21:19:44 +0300

    feat: add JwtService (generate token + parseClaims)

---
+++ src/main/java/ru/zaletnev/mini_twitter/security/JwtService.java
@@ -0,0 +1,61 @@
+package ru.zaletnev.mini_twitter.security;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+    @Value("${jwt.secret}")
+    private String secretBase64;
+    @Value("${jwt.exptime}")
+    private long exptime;
+
+    private SecretKey secretKey;
+
+    @PostConstruct
+    void initKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretBase64);
+        this.secretKey = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = parseClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims parseClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String generateToken(String username) {
+        Date now = new Date();
+        Date exp = new Date(now.getTime() + exptime);
+
+
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(now)
+                .expiration(exp)
+                .signWith(secretKey)
+                .compact();
+    }
+
+}
