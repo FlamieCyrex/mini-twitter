a657fc1e4591efa84e7371d858bb7cf225dee656
FlamieCyrex <wave-5@yandex.ru>
2025-10-23 23:02:01 +0300

    add: add JPA config

---
+++ src/main/java/ru/zaletnev/mini_twitter/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package ru.zaletnev.mini_twitter.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
